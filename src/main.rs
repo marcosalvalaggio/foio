3a1dd4cfa000c7d574e217e6d5748fa81b08a2b0
marcosalvalaggio <mar.salvalaggio@gmail.com>
2023-10-20 17:34:58 +0200

    wip

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,8 @@
 mod command;
 
-
 fn main() {
-    // println!("Hello, seaman!");
-    // command::setup::write_foio_script(true);
-    // command::setup::change_permission_to_foio_script(true);
-    // command::open::execute_foio_script(String::from("10-04-2023"));
+    println!("Hello, seaman!");
+    command::setup::write_foio_script(true);
+    command::setup::change_permission_to_foio_script(true);
+    command::open::execute_foio_script(String::from("10-04-2023"));
 }
