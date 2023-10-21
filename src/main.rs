ba5fd1c8a83b9d061dfd32b3a1fc652ec8247d17
marcosalvalaggio <mar.salvalaggio@gmail.com>
2023-10-22 00:19:20 +0200

    wip

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,28 @@
 mod command;
+use clap::Parser;
+
+#[derive(Debug, Parser)]
+#[clap(author, version)]
+struct Cli {
+    #[clap(short, long, value_parser)]
+    op: String,
+    #[clap(value_parser)]
+    first: f32,
+    #[clap(value_parser)]
+    second: f32
+}
 
 fn main() {
     println!("Hello, seaman!");
     command::setup::write_foio_script(true);
     command::setup::change_permission_to_foio_script(true);
-    command::open::execute_foio_script(String::from("10-04-2023"));
+    command::open::execute_foio_script(String::from("page"));
+    println!("CLI test");
+    let cli = Cli::parse();
+    let result = match cli.op.as_str() {
+        "+" => cli.first + cli.second,
+        "-" => cli.first - cli.second,
+        _ => panic!("lezzo")
+    };
+    println!("{}", result);
 }
