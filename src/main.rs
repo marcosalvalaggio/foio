516ec3974f6edb40389f9420b5be36f8595955f5
marcosalvalaggio <mar.salvalaggio@gmail.com>
2023-10-22 11:47:03 +0200

    wip

--- src/main.rs
+++ src/main.rs
@@ -5,7 +5,7 @@ fn main() {
     println!("Hello seaman!");
     command::init::init_pipeline(true);
     command::setup::setup_pipeline(true);
-    command::open::execute_foio_script(String::from("page"));
+    command::open::execute_foio_script(String::from("_"));
     let cli = command::cli::FoioArgs::parse();
     println!("{:?}", cli);
 }
